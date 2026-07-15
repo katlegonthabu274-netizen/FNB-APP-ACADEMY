881cc864951f7dcda70c4f059300fc46389c5109
katlegonthabu274-netizen <katlego.nthabu274@gmail.com>
2026-07-15 10:36:19 +0200

    Complete Lesson 1

---
+++ Lesson_1.py
@@ -0,0 +1,10 @@
+# Making the computer speak
+print("Hello World!")
+
+# Storing data inside variable
+name = "Kwakhanya"
+print(name)
+
+# Making the code interactive
+name = input("Enter your name: ")
+print(f"Greetings , {name} ")
